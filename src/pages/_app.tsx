051426e24134412e3b47717da94cc1959b94594d
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-04-02 16:30:37 -0300

    creating the header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
-import { globalStyles } from "@/styles/global";
 import { AppProps } from "next/app";
+import { globalStyles } from "@/styles/global";
 
-globalStyles()
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from "../styles/pages/app";
+globalStyles();
 
 export default function App({ Component, pageProps }:AppProps) {
+  return(
+<Container>
+    <Header>
+      <img src={logoImg.src} alt="" />
+    </Header>
+    
+   <Component {...pageProps} />
+  </Container>
+  )
   
-  return <Component {...pageProps} />;
 }
